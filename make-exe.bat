dc37a5a5811de5bade760524b8aa4c9425509719
Elmo Lingad <sesamekuno@gmail.com>
2020-08-15 20:57:39 +0800

    Quick fix on the batch file for the computer

--- make-exe.bat
+++ make-exe.bat
@@ -3,7 +3,7 @@ if exist .\"TicTacToe in Python" (
     rmdir /s /q "TicTacToe in Python"
     echo. && echo Deleted original folder of TicTacToe in Python!
 )
-pyinstaller --onedir TicTacToe-Networking.py --name "TicTacToe in Python" && echo. && echo Successfully created the executable file for TicTacToe in Python!
+pyinstaller --onedir TicTacToe-Computer.py --name "TicTacToe in Python" && echo. && echo Successfully created the executable file for TicTacToe in Python!
 move dist\"TicTacToe in Python" . && echo. && echo Moved directory successfully!
 rmdir dist && echo. && echo Successfully removed dist folder...
 rmdir /s /q build && echo. && echo Successfully removed build folder...
